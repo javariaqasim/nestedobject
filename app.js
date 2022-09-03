98090773c8b80186e7c2f3f976dd1bae61c78c0d
javaria qasim <javariaqasim77@gmail.com>
2022-09-03 16:37:23 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,200 @@
+var products = {
+         Realme:{
+            realme9: {
+                  version: "Android 47.51%",
+                  quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610",
+                  price: "Rs 64,999"
+              },
+              realme8: {
+                version: "Android 47.51%",
+                quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610", 
+                price: "Rs 25500"
+            },
+          realmec11: {
+            version: "Android 47.51%",
+            quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610", 
+            price: "Rs 19000"
+          },
+          realmec12: {
+            version: "Android 47.51%",
+            quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610",
+            price: "Rs 24000"
+          },
+          realmec25: {
+            version: "Android 47.51%",
+            quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610",
+            price: "Rs 30000"
+        },
+        realmec35: {
+            version: "Android 47.51%",
+            quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610",
+            price: "Rs 35000"
+        },
+        realmec31: {
+            version: "Android 47.51%",
+            quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610",
+            price: "Rs: 40000"
+          }
+      },
+  
+      Vivo: {
+          Vivov20: {
+            version: "Android 47.51%",
+            quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+            price: "Rs 50000"
+          },
+          VivoY21: {
+            version: "Android 47.51%",
+            quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+            price: "Rs 70000 "
+        },
+        VivoY33: {
+            version: "Android 47.51%",
+            quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+            price: "Rs 65000"
+        },
+        VivoY75: {
+            version: "Android 47.51%",
+            quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+            price: "Rs 30000"
+        },
+        VivoY12: {
+            version: "Android 47.51%",
+            quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+            price: "Rs 6544"
+        },
+        Vivos5: {
+            version: "Android 47.51%",
+            quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+            price: "Rs 98000"
+        },
+        VivoV15: {
+            version: "Android 47.51%",
+            quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+            price: "Rs 78000"
+        },
+    
+      },
+  
+      iphone: {
+          iphone13: {
+            version: "iOS 41.97%",
+            quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+            price: "Rs 30000"
+          },
+          iphone12: {
+            version: "iOS 41.97%",
+            quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+            price: "Rs 20000"
+        },
+        iphone11: {
+            version: "iOS 41.97%",
+            quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+            price: "Rs 250000"
+        },
+        iphone10: {
+            version: "iOS 41.97%",
+                  quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+                  price: "Rs 145000"
+        },
+        iphone7: {
+            version: "iOS 41.97%",
+            quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+            price: "Rs 8000"
+        },
+        iphone6: {
+            version: "iOS 41.97%",
+            quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+            price: "Rs 40000"
+        },
+  
+      }
+  
+  }
+  
+  let productType = document.getElementById('productType')
+  let productCategory = document.getElementById('productCategory')
+  let productKeys = Object.keys(products) //walets bags
+  var productValues
+  var searchItem
+  var ptInd, pcInd;
+  
+  function fillProductType() {
+      console.log(productKeys)
+      // productType.innerHTML += `<option id="temp">Product Type</option>`
+  
+      for (i = 0; i < productKeys.length; i++) {
+          productType.innerHTML += `<option>${productKeys[i]}</option>`
+      }
+      ptInd = productType.selectedIndex
+  console.log(ptInd)
+  }
+  fillProductType();
+  
+  // if(ptInd==0){
+  //     productType.addEventListener('onclick',function(){
+  //         productValues = Object.keys(Object.values(products)[ptInd]) //{bifold: {…}, long: {…}}
+  //         productCategory.disabled = false;
+      
+  //         for (i = 0; i < productValues.length; i++) {
+  //             productCategory.innerHTML += `<option>${productValues[i]}</option>`
+  //         }
+      
+  //         let search = document.getElementById('search')
+  //         search.disabled = false;
+      
+  
+  //     })
+  // }
+  
+  var executed=false
+  function fillProductCategory() {
+  //   if(executed=false){
+  //     var temp=document.getElementById('temp')
+  //    console.log(temp)
+  
+  //    temp.remove();
+  //    executed=true;
+  // }
+  productCategory.innerHTML=""
+  
+          ptInd = productType.selectedIndex
+          productValues = Object.keys(Object.values(products)[ptInd]) //{bifold: {…}, long: {…}}
+          productCategory.disabled = false;
+      
+          for (i = 0; i < productValues.length; i++) {
+              productCategory.innerHTML += `<option>${productValues[i]}</option>`
+          }
+      
+          let search = document.getElementById('search')
+          search.disabled = false;
+      
+  
+  
+  
+      }
+  
+  
+  function searchfunc() {
+      search.disabled = true;
+      productCategory.disabled = true;
+  // fillProductType();
+      var pcInd = productCategory.selectedIndex
+      let productSpecs = Object.values(Object.values(products)[ptInd])[pcInd]
+      // console.log(productKeys, productValues, productSpecs)
+  
+      let specsTitle = Object.keys(productSpecs)
+      let specsValues = Object.values(productSpecs)
+      console.log(specsValues)
+      let values = document.getElementById('values')
+      for (i = 0; i < specsValues.length; i++) {
+          var li = document.createElement('li')
+          var lip= document.createElement('p')
+          lip.innerHTML = (specsValues[i])
+          li.appendChild(lip)
+          values.appendChild(li)
+      
+      } 
+      productCategory.innerHTML=""
+  
+  }
